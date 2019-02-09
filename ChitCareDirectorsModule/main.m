9fdc1d04deb092f0d225fd01b14e8fc98318435c
Karishma Upadhyay <41079940+karishma10091993@users.noreply.github.com>
2019-02-09 17:18:49 +0530

    Add files via upload

---
+++ ChitCareDirectorsModule/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChitCareDirectorsModule
+//
+//  Created by kireeti on 11/04/14.
+//  Copyright (c) 2014 Madhavi KireetiSoft technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
